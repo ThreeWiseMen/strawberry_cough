d3f0a54933bfb3423cb5d5ce8b4de11741327f18
Nate Smith <nwjsmith@gmail.com>
2010-09-15 15:28:48 -0400

    Parse path anchors.

--- lib/strawberry_cough/path_parser.rb
+++ lib/strawberry_cough/path_parser.rb
@@ -1,9 +1,14 @@
 module StrawberryCough
   class PathParser
     PARAM_PATTERN = /:([\w_\d]+)/
+    ANCHOR_PATTERN = /\/([\w_\d]+)/
 
     def self.parse_params(path)
       path.scan(PARAM_PATTERN).flatten
+    end
+
+    def self.parse_anchors(path)
+      path.scan(ANCHOR_PATTERN).flatten
     end
   end
 end
--- spec/path_parser_spec.rb
+++ spec/path_parser_spec.rb
@@ -2,14 +2,26 @@ require 'spec_helper'
 
 describe StrawberryCough::PathParser do
   let(:parser) { StrawberryCough::PathParser }
+  let(:simple_resource_path) { "/parent/:id/edit(.:format)" }
+  let(:nested_resource_path) { "/parent/:parent_id/child/:id/edit(.:format)" }
 
-  it "extracts parameters from simple resource path" do
-    simple_resource_path = "/parent/:id/edit(.:format)"
-    parser.parse_params(simple_resource_path).should == %w{id format}
+  describe ".parse_params" do
+    it "parses parameters from simple resource path" do
+      parser.parse_params(simple_resource_path).should == %w{id format}
+    end
+
+    it "parses parameters from nested resource path" do
+      parser.parse_params(nested_resource_path).should == %w{parent_id id format}
+    end
   end
 
-  it "extracts parameters from nested resource path" do
-    nested_resource_path = "/parent/:parent_id/child/:id/edit(.:format)"
-    parser.parse_params(nested_resource_path).should == %w{parent_id id format}
+  describe ".parse_anchors" do
+    it "parses anchors from a simple resource path" do
+      parser.parse_anchors(simple_resource_path).should == %w{parent edit}
+    end
+
+    it "parses anchors from a nested resource path" do
+      parser.parse_anchors(nested_resource_path).should == %w{parent child edit}
+    end
   end
 end
