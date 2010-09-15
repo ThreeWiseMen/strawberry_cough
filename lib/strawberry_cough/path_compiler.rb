ba0bbdc3d14838ace87c6000b2bad52832772cc3
Nate Smith <nwjsmith@gmail.com>
2010-09-15 15:55:02 -0400

    Compile paths to JS functions.

--- lib/strawberry_cough/path_compiler.rb
+++ lib/strawberry_cough/path_compiler.rb
@@ -1,4 +1,19 @@
 module StrawberryCough
   class PathCompiler
+    def self.compile(path)
+      params  = PathParser.parse_params(path)
+      anchors = PathParser.parse_anchors(path)
+      anchors.map! { |a| %Q{"/#{a}/"} }
+
+      params_without_format = params.reject { |p| p == "format" }
+
+      url = anchors.zip(params_without_format).flatten.compact.join(" + ").gsub(/\/"$/, '"')
+      js = <<-JS
+        function(#{params.join(", ")}) {
+          var url = #{url};
+          return ( format !== undefined ) ? url + "." + format : url;
+        };
+      JS
+    end
   end
 end
--- spec/path_compiler_spec.rb
+++ spec/path_compiler_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe StrawberryCough::PathCompiler do
+  let(:compiler) { StrawberryCough::PathCompiler }
+  let(:simple_resource_path) { "/parent/:id/edit(.:format)" }
+
+  it "compiles a simple resource path into a JavaScript function" do
+    js = <<-JS
+        function(id, format) {
+          var url = "/parent/" + id + "/edit";
+          return ( format !== undefined ) ? url + "." + format : url;
+        };
+    JS
+    compiler.compile(simple_resource_path).should == js
+  end
 end
