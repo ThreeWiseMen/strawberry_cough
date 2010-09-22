540c20b89ca0f572043d3d01af63de2e358e023d
Craig Savolainen and Nate Smith <nwjsmith+maedhr+nwjsmith@gmail.com>
2010-09-22 14:03:53 -0400

    Railtie needs the application.

--- lib/strawberry_cough/railtie.rb
+++ lib/strawberry_cough/railtie.rb
@@ -1,12 +1,13 @@
-require 'routes_compiler'
+require 'strawberry_cough/routes_compiler'
 
 module StrawberryCough
   class Railtie < Rails::Railtie
-    config.to_prepare do |app|
+    config.to_prepare do
+      app = Rails.application
       routes = app.routes.routes
       file = File.join(app.root, "public", "javascripts", "routes.js")
       File.open(file, "w") do |io|
-        RoutesCompiler.compile_to_io(route_set, io)
+        RoutesCompiler.compile_to_io(routes, io)
       end
     end
   end
