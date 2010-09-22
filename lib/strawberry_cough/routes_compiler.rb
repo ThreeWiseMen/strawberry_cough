0429cdb42e2f2afde9d7e2720d5b7b68254e9cf9
Craig Savolainen and Nate Smith <nwjsmith+maedhr+nwjsmith@gmail.com>
2010-09-22 14:27:13 -0400

    Use a set to avoid duplication

--- lib/strawberry_cough/routes_compiler.rb
+++ lib/strawberry_cough/routes_compiler.rb
@@ -4,11 +4,11 @@ module StrawberryCough
   class RoutesCompiler
 
     def self.compile(route_set)
-      functions = route_set.inject([]) do |memo, route|
+      functions = route_set.inject(Set.new) do |memo, route|
         path_func_name = route.name.camelize(:lower) + "Path"
         memo << "this.#{path_func_name} = " + PathCompiler.compile(route.path)
       end
-      "var StrawberryCough = {#{functions.uniq.join}};"
+      "var StrawberryCough = {#{functions.to_a.join}};"
     end
 
     def self.compile_to_io(route_set, io)
