b2a9f99c544ebaad170f3a2fb3418a4bc4f5b6c2
Nate Smith <nwjsmith@gmail.com>
2011-06-22 16:27:23 -0400

    Test against dummy application

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'aadd9f8f25156af7d32a19e37d6b706edd54f59b9acf1b6d3037227cfa9dc8f71dfe3c9bf5d67678f03de538117e721920a499d42659309b88c7c1bb6e1cb00b'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,18 @@
-lib_dir = File.dirname(__FILE__) + '/../lib'
-$:.unshift lib_dir unless $:.include?(lib_dir)
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
 
-require 'rspec'
+lib_dir = File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
+
+require File.expand_path("../dummy/config/environment", __FILE__)
+require 'rspec/rails'
 require 'v8'
 
 require 'strawberry_cough'
 
 include StrawberryCough
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/{support,shared}/**/*.rb")].each { |f| require f }
 
